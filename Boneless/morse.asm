3c582bed26da29efbd407757b03370c3e9d2aaa7
Simon Kirkby <obeygiantrobot@gmail.com>
2019-03-25 12:13:43 +0800

    more morse work

--- Boneless/morse.asm
+++ Boneless/morse.asm
@@ -1,3 +1,111 @@
+; Morse code flasher
+; Simon Kirkby
+; obeygiantrobot@gmail.com
+; 20190325
+;
+
+; name the register for work.
+
+.def DATA, R0
+.def ADDR, R1
+.def COUNTER, R2
+.def MAX , R3
+.def OUT, R4
+.def LETTER , R5
+
+.def RTN, R7
+
+; helper macros
+.macro _call,val
+    JAL RTN, $val
+.endm
+
+.macro _return
+    JR RTN,0
+.endm
+
+.macro BRK
+    STX OUT,OUT,1
+.endm
+
+.macro out
+    STX DATA,OUT,0
+.endm
+
+; Jump to init 
+J init
+NOP
+
+; The message to display
+.string message,"think global act local"
+
+alpha_address:
+    .@ alpha
+
+init:
+    MOVA ADDR, message
+    LD MAX, ADDR ,0
+    ADD MAX,ADDR,MAX
+char_loop:
+    LD DATA, ADDR ,0
+    out
+    _call morse_char
+    CMP ADDR,MAX
+    JE char_loop_out
+    ADDI ADDR,1
+    J char_loop
+char_loop_out:
+    BRK
+J init
+
+morse_char: ; output each char
+    SUBI DATA,97 ; off set for 'a', move back to zero
+    MOVA LETTER,alpha
+    ADD LETTER,DATA,LETTER
+;    LD DATA,LETTER,0
+_return
+
+; letter and numbers map
+alpha:
+    .@ a
+    .@ b
+    .@ c
+    .@ d
+    .@ e
+    .@ f
+    .@ g
+    .@ h
+    .@ i
+    .@ j
+    .@ k
+    .@ l
+    .@ m
+    .@ n
+    .@ o
+    .@ p
+    .@ q
+    .@ r
+    .@ s
+    .@ t
+    .@ u
+    .@ v
+    .@ w
+    .@ x
+    .@ y
+    .@ z
+number:
+    .@ D0
+    .@ D1
+    .@ D2
+    .@ D3
+    .@ D4
+    .@ D6
+    .@ D7
+    .@ D8
+    .@ D9
+NOP
+NOP
+; Morse code letters
 .string a,".-"
 .string b,"-..."
 .string c,"-.-."
@@ -34,3 +142,6 @@
 .string D7,"--..."
 .string D8,"---.."
 .string D9,"----."
+
+
+
