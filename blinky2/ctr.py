59d64b533d966b21e7ef1eff3a5b006e69dee6ce
Simon Kirkby <obeygiantrobot@gmail.com>
2019-03-26 10:48:32 +0800

    need to convert migen -> nmigen for platform

--- blinky2/ctr.py
+++ blinky2/ctr.py
@@ -2,9 +2,30 @@ from nmigen import *
 from nmigen.cli import main, pysim
 
 
+class Pin:
+    def __init__(self):
+        self.io = Signal()
+        self.o = Signal()
+        self.oe = Signal()
+        self.i = Signal()
+
+    def elaborate(self, platform):
+        m = Module()
+        p = Instance(
+            "SB_IO",
+            p_PIN_TYPE=C(0b101001, 6),
+            io_PACKAGE_PIN=self.io,
+            i_OUTPUT_ENABLE=self.oe,
+            i_D_OUT_0=self.o,
+            o_D_IN_0=self.i,
+        )
+        m.submodules += p
+        return m
+
+
 class Counter:
     def __init__(self, width):
-        self.v = Signal(width, reset=2**width-1)
+        self.v = Signal(width, reset=2 ** width - 1)
         self.o = Signal()
 
     def elaborate(self, platform):
@@ -15,5 +36,6 @@ class Counter:
 
 
 ctr = Counter(width=16)
+p = Pin()
 if __name__ == "__main__":
-    main(ctr, ports=[ctr.o])
+    main(p, ports=[p.o])
