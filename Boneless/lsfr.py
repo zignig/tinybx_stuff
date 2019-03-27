8f4ac37972cb086a8879267cce9441642a3c63bb
Simon Kirkby <obeygiantrobot@gmail.com>
2019-03-27 14:38:20 +0800

    bigger stretch

--- Boneless/lsfr.py
+++ Boneless/lsfr.py
@@ -8,12 +8,12 @@ from random import randint
 #  collection of mls , https://users.ece.cmu.edu/~koopman/lfsr
 
 class lsfr:
-    def __init__(self,width=31,taps=[27,30],initial=50,s=20):
+    def __init__(self,width=31,taps=[27,30],initial=50,s=1024):
         self.o = Signal()
         self.width = width
         self.taps = taps
         self.initial = initial
-        self.stretcher = Signal(8)
+        self.stretcher = Signal(16)
         self.stretch = s
 
     def elaborate(self,platform):
