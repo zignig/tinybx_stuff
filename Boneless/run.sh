2fc1f373335857d0e960225fbe79c0b06d9d1af7
Simon Kirkby <obeygiantrobot@gmail.com>
2019-03-27 15:03:54 +0800

    fixed pcf file ref

--- Boneless/run.sh
+++ Boneless/run.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 python3 processor.py generate build/fsm_core_pins.v
 yosys build/fsm_core_pins.v tinyBX.sv -p "synth_ice40 -top top -json build/tinyBX.json"
-nextpnr-ice40 --placer heap --hx8k --package cm81 --pcf build/tinyBX.pcf --json build/tinyBX.json --asc build/tinyBX.txt
+nextpnr-ice40 --placer heap --hx8k --package cm81 --pcf tinyBX.pcf --json build/tinyBX.json --asc build/tinyBX.txt
 icepack build/tinyBX.txt build/tinyBX.bin
 tinyprog -p build/tinyBX.bin
 
