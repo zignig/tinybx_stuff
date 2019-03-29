cc9c7b0e6bdc110dc91a6ececbf58fbd6ffb5e43
Simon Kirkby <obeygiantrobot@gmail.com>
2019-03-29 10:51:43 +0800

    failing usb

--- Boneless/boneless_tbx.v
+++ Boneless/boneless_tbx.v
@@ -1,69 +1,65 @@
 module boneless_tbx(
-  inout  pin_usb_p,
-  inout  pin_usb_n,
-  output pin_pu,
+    inout  pin_usb_p,
+    inout  pin_usb_n,
+    output pin_pu,
     input clk,
     output [3:0] led
 );
-  wire clk_48mhz;
+    wire clk_48mhz;
+    wire clk_locked;
+    // Use an icepll generated pll
+    pll pll48( .clock_in(clk), .clock_out(clk_48mhz), .locked( clk_locked ) );
 
-  wire clk_locked;
+    // Generate reset signal
+    reg [5:0] reset_cnt = 0;
+    wire reset = ~reset_cnt[5];
+    always @(posedge clk_48mhz)
+        if ( clk_locked )
+            reset_cnt <= reset_cnt + reset;
+    // -------------------------
+    // USB serial
+    // interface to the boneless cpu
+    wire [2:0] usb_in_control;
+    wire [7:0] usb_in_data;
+    wire [2:0] usb_out_control;
+    wire [7:0] usb_out_data;
+    //assign debug = { uart_in_valid, uart_in_ready, reset, clk_48mhz };
 
-  // Use an icepll generated pll
-  pll pll48( .clock_in(clk), .clock_out(clk_48mhz), .locked( clk_locked ) );
+    // usb uart - this instanciates the entire USB device.
+    usb_uart_i40 uart (
+        .clk_48mhz  (clk_48mhz),
+        .reset      (reset),
 
-  // Generate reset signal
-  reg [5:0] reset_cnt = 0;
-  wire reset = ~reset_cnt[5];
-  always @(posedge clk_48mhz)
-      if ( clk_locked )
-          reset_cnt <= reset_cnt + reset;
-// -------------------------
-// USB serial
+        // pins
+        .pin_usb_p( pin_usb_p ),
+        .pin_usb_n( pin_usb_n ),
 
-  // uart pipeline in
-  wire [7:0] uart_in_data;
-  wire       uart_in_valid;
-  wire       uart_in_ready;
-  assign debug = { uart_in_valid, uart_in_ready, reset, clk_48mhz };
+        // uart pipeline in
+        .uart_in_data( usb_in_data),
+        .uart_in_valid( usb_in_control[0]),
+        .uart_in_ready( usb_in_control[1]),
 
-  wire usb_p_tx;
-  wire usb_n_tx;
-  wire usb_p_rx;
-  wire usb_n_rx;
-  wire usb_tx_en;
-  // usb uart - this instanciates the entire USB device.
-  usb_uart_i40 uart (
-    .clk_48mhz  (clk_48mhz),
-    .reset      (reset),
+        .uart_out_data( usb_out_data),
+        .uart_out_valid( usb_out_control[0]),
+        .uart_out_ready( usb_out_control[1])
 
-    // pins
-    .pin_usb_p( pin_usb_p ),
-    .pin_usb_n( pin_usb_n ),
-
-    // uart pipeline in
-    .uart_in_data( uart_in_data ),
-    .uart_in_valid( uart_in_valid ),
-    .uart_in_ready( uart_in_ready ),
-
-    .uart_out_data( uart_in_data ),
-    .uart_out_valid( uart_in_valid ),
-    .uart_out_ready( uart_in_ready  )
-
-    //.debug( debug )
-  );
-
-  assign pin_pu = 1'b1;
+        //.debug( debug )
+    );
 
 
-// Boneless CPU
-// -------------------------
+    assign pin_pu = 1'b1;
 
+    // Boneless CPU
+    // -------------------------
     boneless_core cpu(
         .clk(clk_48mhz),
         .rst(reset),
         .r_win(0),
+        .usb_in_control(usb_in_control),
+        .usb_out_control(usb_out_control),
+        .usb_in_data(usb_in_data),
+        .usb_out_data(usb_out_data),
         .pins(led),
     );
-// -------------------------
+    // -------------------------
 endmodule
--- Boneless/processor.py
+++ Boneless/processor.py
@@ -12,6 +12,11 @@ class Boneless:
         self.ext_port = _ExternalPort()
         self.pins = Signal(16, name="pins") if has_pins else None
 
+        self.usb_in_control = Signal(16,name="usb_in_control")
+        self.usb_in_data = Signal(16,name="usb_in_data")
+        self.usb_out_control = Signal(16,name="usb_out_control")
+        self.usb_out_data = Signal(16,name="usb_out_data")
+
         code = Assembler(file_name=asmfile)
         code.assemble()
         self.memory.init = code.code
@@ -20,21 +25,39 @@ class Boneless:
         m = Module()
 
         if self.pins is not None:
+            # blinky on port address 0
             with m.If(self.ext_port.addr == 0):
                 with m.If(self.ext_port.r_en):
                     m.d.sync += self.ext_port.r_data.eq(self.pins)
                 with m.If(self.ext_port.w_en):
                     m.d.sync += self.pins.eq(self.ext_port.w_data)
 
+            # usb control data
+            with m.If(self.ext_port.addr == 256):
+                with m.If(self.ext_port.r_en):
+                   m.d.sync += self.ext_port.r_data.eq(self.usb_in_control)
+            # usb data
+            with m.If(self.ext_port.addr == 255):
+                with m.If(self.ext_port.r_en):
+                    m.d.sync += self.ext_port.r_data.eq(self.usb_in_data)
+            # usb control data
+            with m.If(self.ext_port.addr == 254):
+                with m.If(self.ext_port.w_en):
+                    m.d.sync += self.usb_out_control.eq(self.ext_port.w_data)
+            # usb data
+            with m.If(self.ext_port.addr == 253):
+                with m.If(self.ext_port.w_en):
+                    m.d.sync += self.usb_out_data.eq(self.ext_port.w_data)
+
         m.submodules.mem_rdport = mem_rdport = self.memory.read_port(transparent=False)
-        m.submodules.mem_wdport = mem_wrport = self.memory.write_port()
+        m.submodules.mem_wrport = mem_wrport = self.memory.write_port()
         m.submodules.core = BonelessCoreFSM(
             reset_addr=8,
             mem_rdport=mem_rdport,
             mem_wrport=mem_wrport,
             ext_port=self.ext_port,
         )
-        return m.lower(platform)
+        return m
 
 if __name__ == "__main__":
     import argparse
@@ -44,6 +67,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     tb= Boneless(has_pins=True)
-    ios = (tb.pins,)
+    ios = (tb.pins,tb.usb_in_control,tb.usb_in_data,tb.usb_out_control,tb.usb_out_data)
 
     cli.main_runner(parser,args,tb,name="boneless_core",ports=ios)
