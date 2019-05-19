0d06ed1f4f50f4e7abb47a5aeb8e8c1792fb8c6b
Simon Kirkby <obeygiantrobot@gmail.com>
2019-05-19 08:07:53 +0800

    wrap up lsfr , test 1

---
+++ blinky2/collate.py
@@ -0,0 +1,23 @@
+# linear shift feedback register periph
+
+#  collection of mls , https://users.ece.cmu.edu/~koopman/lfsr
+import os
+def gen_output():
+    sizes = os.listdir('max_len_seq/')
+    seq = {}
+    for i in sizes:
+        if i.endswith('.txt'):
+            t = int(i[:-4])
+            f = open('max_len_seq/'+i)
+            li = f.readlines()
+            f.close()
+            tmp = []
+            for j in li:
+                tmp.append(j.strip())
+            seq[t] = tmp
+    return seq        
+
+a = {}
+if __name__ == "__main__":
+    a = gen_output()
+    print(a)
