cb73974e3b34d79bc99ce995fff834eb74261667
Simon Kirkby <obeygiantrobot@gmail.com>
2019-06-07 09:27:45 +0800

    bip - boop - bip -boop

--- platform_test/cores/larson.py
+++ platform_test/cores/larson.py
@@ -2,19 +2,36 @@ from nmigen import *
 from nmigen.cli import main, pysim
 
 class Larson(Elaboratable):
-    def __init__(self,width=5):
+    def __init__(self,width=5,stretch=10):
         # width of the scanner
         self.width = width
         self.enable = Signal(reset=1)
-
+        self.track = Signal(width+2,reset=1)
+        self.stretcher = Signal(max=stretch)
+        self.stretch = stretch
+        self.updown = Signal(reset=1)
 
     def elaborate(self,platform):
         m = Module()
         with m.If(self.enable):
-             
+            m.d.sync += self.stretcher.eq(self.stretcher + 1)
+            with m.If(self.stretcher == self.stretch):
+                m.d.sync +=  self.stretcher.eq(0)
+                with m.If(self.updown):
+                    with m.If(self.track[self.width+1] == 1):
+                        #m.d.sync += self.track[0].eq(1)
+                        m.d.sync += self.updown.eq(0)
+                    with m.Else():
+                        m.d.sync +=  self.track.eq(self.track << 1) 
+                with m.If(~self.updown):
+                    with m.If(self.track[0] == 1):
+                        m.d.sync += self.updown.eq(1)
+                    with m.Else():
+                        m.d.sync += self.track.eq(self.track >> 1)
+
         return m
         
 if __name__ == "__main__":
-    b = Larson(width=5)
-    pins = (b.counter,b.value,b.enable,b.o,b.updown,b.top,b.bottom)
+    b = Larson(width=4,stretch=50)
+    pins = (b.track,b.stretcher,b.stretch,b.updown)
     main(b,pins,name="top")
