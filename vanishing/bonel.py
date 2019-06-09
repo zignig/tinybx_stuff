8b94a536455b1558b2638f9d262f2e5ca96cb199
Simon Kirkby <obeygiantrobot@gmail.com>
2019-06-09 13:47:26 +0800

    add output device, then synths properly.

--- vanishing/bonel.py
+++ vanishing/bonel.py
@@ -6,13 +6,15 @@ from nmigen_boards.tinyfpga_bx import TinyFPGABXPlatform
 class boneless_core(Elaboratable):
     def elaborate(self, platform):
         clk16    = platform.request("clk16", 0)
+        user_led = platform.request("user_led", 0)
 
         m = Module()
         m.domains.sync  = ClockDomain()
         m.d.comb += ClockSignal().eq(clk16.i)
         
-        cpu = BonelessFSMTestbench()
+        cpu = BonelessFSMTestbench(has_pins=True)
 
+        m.d.sync += user_led.eq(cpu.pins[0])
         m.submodules.cpu = cpu
 
         return m
