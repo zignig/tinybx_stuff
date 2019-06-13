12cab2f523bf4f005522355e958e52a2a04ca584
Simon Kirkby <obeygiantrobot@gmail.com>
2019-06-13 14:52:17 +0800

    how do I get signed numbers into memory ?

--- platform_test/cores/trig.py
+++ platform_test/cores/trig.py
@@ -1,24 +1,53 @@
 from nmigen import * 
+from nmigen.cli import pysim
 
 import math 
 
 class Sin(Elaboratable):
-    def __init__(self,width=16,resolution=16):
-        self.table = Memory(width=width,depth=resolution)
+    def __init__(self,width=16,resolution=8):
+        self.table = Memory(width=width,depth=2**resolution)
+        self.o = Signal((width,True))
         self.resolution = resolution
 
+        self.tick = 50
+        self.counter = Signal(resolution)
         # make the sin table
         points = []
-        inc = 1.0/resolution
+        inc = 1.0/(2**resolution)
         scale = 2**width
         print(scale)
-        for i in range(resolution):
+        for i in range(2**resolution):
             val = math.sin(2*math.pi*inc*i)
             print(i,val)
             points.append(round(val*scale))
         print(points)
         self.table.init = points
 
+    def elaborate(self,platform):
+        m = Module()
+        m.d.sync += self.counter.eq(self.counter+1)
+        #m.d.comb += self.o.eq(self.table[self.counter])
+        return m 
+        
+
+def tail():
+    for i in range(1000):
+        yield 
+
+def runner():
+    yield from tail()
+
 if __name__ == "__main__":
-    a = Sin(resolution=32)
+    tb  = Sin()
+    freq = 9600
+    print(dir(tb))
+    with pysim.Simulator(
+        tb,
+        vcd_file=open("trig.vcd", "w"),
+        #gtkw_file=open("trig.gtkw", "w"),
+        #traces=[tb.o,tb.counter],
+    ) as sim:
+        sim.add_clock(freq)
+        sim.add_sync_process(runner)
+        sim.run()
 
