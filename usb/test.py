708ca034f82f011c949d73967648f5d6e1f87c95
Simon Kirkby <obeygiantrobot@gmail.com>
2019-10-25 13:33:39 +0800

    add test.py

---
+++ usb/test.py
@@ -0,0 +1,20 @@
+import time
+import usb.core
+
+
+def main():
+    dev = usb.core.find(idVendor=0x05ac, idProduct=0x5678)
+    if dev is None:
+        raise ValueError("Device 0x05ac:0x5678 was not found.")
+
+    dev.set_configuration()
+
+    led = 0b1
+    for i in range(32):
+        dev.write(1, led.to_bytes(1, byteorder='big'), 1)
+        print(f"[OUT] data = {led}");
+        time.sleep(1) # wait 1s to see the led change its color
+
+
+if __name__ == "__main__":
+    main()
